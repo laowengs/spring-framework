4a51f9d193bbc63f61d0b573f2da9dc585f4b6fa
laoweng <229595412@qq.com>
2021-01-13 17:35:25 +0800

    ClassPathXmlApplicationContext

--- spring-demo/src/main/java/org/springframework/demo/SpringTest.java
+++ spring-demo/src/main/java/org/springframework/demo/SpringTest.java
@@ -4,10 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 public class SpringTest {
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("");
 		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(TestConfig.class);
 		TestProcessor testProcessor = applicationContext.getBean(TestProcessor.class);
 		testProcessor.process();
